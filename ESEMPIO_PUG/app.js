8bd49026e8b138223f1e4f47734bd1ad328bcfe1
MolodduwaGabriel <molodduwa.quiroz.gabriel.andres@iisgalvanimi.edu.it>
2019-11-06 10:46:43 +0000

    asdas

--- ESEMPIO_PUG/app.js
+++ ESEMPIO_PUG/app.js
@@ -1,15 +1,24 @@
+const express = require('express');
+const people = require('./people.json'); //Copia il file people.json dentro la variabile people
+const app = express();
 
-var express = require('express');
-var app = express();
+app.set('view engine', 'pug');
+app.use(express.static(__dirname + '/public'));
 
-app.set('view engine', 'pug');   //Dico a express di usare pug come motore di template
-
-app.use(express.static(__dirname + '/public')); // Dico ad express dove recuperare i file statici
-
-app.get('/', function (req, res) {
- //res.send('Ciao Mondo');
- res.render('index');  //Dico a express di processare e inviare la pagina index.pug
+app.get('/', (req, res) => {
+  res.render('index', {
+    title: 'Homepage',
+    people: people.profiles //Passa il vettore profiles alla pagina index.pug
+  });
 });
+app.get('/profile', (req, res) => {
+  const person = people.profiles.find(p => p.id === req.query.id);
+  res.render('profile', {
+    title: `About ${person.firstname} ${person.lastname}`,
+    person,
+  });
+});
+
 app.listen(3000, function () {
  console.log('Example app listening on port 3000!');
 });
